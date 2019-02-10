d2189cc113f6bc5332db67075c0f6e778f69d721
KDragon <elkarouani@gmail.com>
2019-02-10 22:28:07 +0100

    Task: adding mongodb connection methods

--- db.js
+++ db.js
@@ -2,4 +2,25 @@ const MongoClient = require("mongoDB").MongoClient;
 const ObjectID = require("mongoDB").ObjectID;
 const dbname = "crud_mongodb";
 const url = "mongodb://localhost:27017";
-const mongoOptions = {urlParser : true}; 
+const mongoOptions = {urlParser : true};
+
+const state = {
+	db : null
+};
+
+const connect = (cb) => {
+	if (state.db) {cb();} else {
+		MongoClient.connect(url, mongoOptions, (err, client) => {
+			if (err) {cb(err);} else {
+				state.db = client.db(dbname);
+				cb();
+			}
+		});
+	}
+}
+
+const getPrimaryKey = (_id) => ObjectID(_id);
+
+const getDB = () => state.db;
+
+module.exports = {getDB, connect, getPrimaryKey};
