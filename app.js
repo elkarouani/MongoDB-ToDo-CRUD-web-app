fce9d9e13e2def200e69c52246f4e076bb845274
KDragon <elkarouani@gmail.com>
2019-02-11 22:02:10 +0100

    Task: getting data from mongodb database

--- app.js
+++ app.js
@@ -7,6 +7,19 @@ const collection = "todo";
 
 app.use(bodyParser.json());
 
+app.get('/', (req, res)=>{
+	res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.get('/getTodos', (req, res)=>{
+	db.getDB().collection(collection).find({}).toArray((err, documents)=>{
+		if (err) {console.log(err)} else {
+			console.log(documents);
+			res.json(documents);
+		}
+	});
+});
+
 db.connect((err) => {
 	if (err) {
 		console.log('unable to connect to database');
