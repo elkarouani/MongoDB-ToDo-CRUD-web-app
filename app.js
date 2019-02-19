7e4ba5a6bca37e01feec76507ac0ee27be37bb52
KDragon <elkarouani@gmail.com>
2019-02-19 19:11:38 +0100

    Task: adding a verifing input schema with possibilty diplaying alerts on creating tasks method*

--- app.js
+++ app.js
@@ -4,8 +4,21 @@ const app = express();
 const path = require('path');
 const db = require("./db");
 const collection = "todo";
+const Joi = require('joi');
+
+const schema = Joi.object().keys({
+	todo : Joi.string().required()
+})
 
 app.use(bodyParser.json());
+
+app.use((err, req, res, next)=>{
+	res.status(err.status).json({
+		error : {
+			message : err.message
+		}
+	});
+});
 
 app.get('/', (req, res)=>{
 	res.sendFile(path.join(__dirname, 'index.html'));
@@ -23,17 +36,31 @@ app.get('/getTodos', (req, res)=>{
 app.put('/:id', (req, res)=>{
 	const todoID = req.params.id;
 	const userInput = req.body;
-
 	db.getDB().collection(collection).findOneAndUpdate({_id : db.getPrimaryKey(todoID)}, {$set : {todo : userInput.todo} }, {returnOriginal : false}, (err, result) => {
 		if (err) {console.log(err);} else {res.json(result);}
 	}) 
 });
 
-app.post('/', (req, res) => {
+app.post('/', (req, res, next) => {
 	const userInput = req.body;
-	db.getDB().collection(collection).insertOne(userInput, (err, result) => {
-		if (err) {console.log(err);} else {res.json({result : result, document : result.ops[0]});}
-	});
+
+	Joi.validate(userInput, schema, (err, result)=>{
+		if (err) {
+			const error = new Error("Invalid Input");
+			error.status = 400;
+			next(error);
+		} else {
+			db.getDB().collection(collection).insertOne(userInput, (err, result) => {
+				if (err) {
+					const error = new Error("Failed to insert Todo Document");
+					error.status = 400;
+					next(error);
+				} else {
+					res.json({result : result, document : result.ops[0], msg : "Successfully inserted Task", error : null});
+				}
+			});
+		}
+	})
 });
 
 app.delete('/:id', (req, res)=>{
